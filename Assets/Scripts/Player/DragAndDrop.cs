b9c9ead567261cc573ec0d63d224b33a493a6876
Xwilarg <xwilarg@protonmail.com>
2022-05-06 20:47:42 +0200

    Add ability to throw objects when ending dnd

--- Assets/Scripts/Player/DragAndDrop.cs
+++ Assets/Scripts/Player/DragAndDrop.cs
@@ -9,7 +9,7 @@ namespace GamedevGBG
         private Camera _camera;
 
         private Transform _dragTarget;
-        private Vector3 _offset;
+        private Vector3 _lastFramePos;
 
         private void Start()
         {
@@ -18,9 +18,10 @@ namespace GamedevGBG
 
         private void Update()
         {
-            if (_dragTarget != null)
+            if (_dragTarget != null) // TODO: Improve drag & drop
             {
-                Vector3 mouseWorld = _camera.ScreenToWorldPoint(new Vector3(_mousePos.x, _mousePos.y, Vector3.Distance(transform.position, _camera.transform.position)));
+                _lastFramePos = _dragTarget.position;
+                Vector3 mouseWorld = _camera.ScreenToWorldPoint(new Vector3(_mousePos.x, _mousePos.y, -_camera.transform.position.z));
                 _dragTarget.transform.position = new Vector3(
                     x: mouseWorld.x,
                     y: mouseWorld.y,
@@ -36,19 +37,20 @@ namespace GamedevGBG
 
         public void OnClick(InputAction.CallbackContext value)
         {
-            Debug.Log(value.phase);
             if (value.phase == InputActionPhase.Started)
             {
                 var ray = _camera.ScreenPointToRay(_mousePos);
-                if (Physics.Raycast(ray, out RaycastHit hit, float.PositiveInfinity))
+                if (Physics.Raycast(ray, out RaycastHit hit, float.PositiveInfinity) && hit.collider.CompareTag("Draggable"))
                 {
                     _dragTarget = hit.collider.transform;
                     _dragTarget.GetComponent<Rigidbody>().isKinematic = true;
+                    _dragTarget.transform.rotation = Quaternion.identity;
                 }
             }
-            else if (value.phase == InputActionPhase.Canceled)
+            else if (value.phase == InputActionPhase.Canceled && _dragTarget != null)
             {
                 _dragTarget.GetComponent<Rigidbody>().isKinematic = false;
+                _dragTarget.GetComponent<Rigidbody>().AddForce((_dragTarget.position - _lastFramePos) * 10f, ForceMode.Impulse);
                 _dragTarget = null;
             }
         }
