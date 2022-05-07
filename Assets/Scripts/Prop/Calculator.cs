2fd5ae4e5b37ee2f5daae47169e0621f2e6c9d87
Xwilarg <xwilarg@protonmail.com>
2022-05-07 12:33:24 +0200

    Add calculator

---
+++ Assets/Scripts/Prop/Calculator.cs
@@ -0,0 +1,126 @@
+﻿using System.Globalization;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace GamedevGBG.Prop
+{
+    public class Calculator : MonoBehaviour
+    {
+        private string _number = string.Empty;
+        private float _total;
+        private Operation _nextOperation = Operation.Sum;
+
+        [SerializeField]
+        private TMP_Text _result;
+
+        private void Start()
+        {
+            CultureInfo customCulture = (CultureInfo)Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            Thread.CurrentThread.CurrentCulture = customCulture;
+        }
+
+        private void ReadyNextOperation(Operation op)
+        {
+            switch (_nextOperation)
+            {
+                case Operation.Add:
+                    _total += float.Parse(_number);
+                    break;
+
+                case Operation.Subscract:
+                    _total -= float.Parse(_number);
+                    break;
+
+                case Operation.Multiply:
+                    _total *= float.Parse(_number);
+                    break;
+
+                case Operation.Divide:
+                    if (float.Parse(_number) == 0)
+                    {
+                        // Ohno
+                    }
+                    else
+                    {
+                        _total /= float.Parse(_number);
+                    }
+                    break;
+
+                case Operation.Sum:
+                    _total = float.Parse(_number);
+                    _number = string.Empty;
+                    break;
+            }
+
+            _nextOperation = op;
+            _result.text = _total.ToString();
+            _number = string.Empty;
+        }
+
+        public void Add(char nb)
+        {
+            if (nb == '.')
+            {
+                if (!_number.Contains("."))
+                {
+                    _number = ".";
+                }
+            }
+            else if (nb == '+')
+            {
+                ReadyNextOperation(Operation.Add);
+            }
+            else if (nb == '-')
+            {
+                ReadyNextOperation(Operation.Subscract);
+            }
+            else if (nb == '/')
+            {
+                ReadyNextOperation(Operation.Divide);
+            }
+            else if (nb == '*')
+            {
+                ReadyNextOperation(Operation.Multiply);
+            }
+            else if (nb == '=')
+            {
+                ReadyNextOperation(Operation.Sum);
+            }
+            else
+            {
+                _number += nb;
+            }
+
+            if (_number != string.Empty)
+            {
+                _result.text = _number;
+            }
+        }
+
+        public void OnClick(InputAction.CallbackContext value)
+        {
+            if (value.performed)
+            {
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue()), out RaycastHit hit))
+                {
+                    if (hit.collider.name.StartsWith("CALC_"))
+                    {
+                        Add(hit.collider.name[5]);
+                    }
+                }
+            }
+        }
+
+        private enum Operation
+        {
+            Add,
+            Multiply,
+            Divide,
+            Subscract,
+            Sum
+        }
+    }
+}
