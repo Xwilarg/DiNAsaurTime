7073de8fbb1328d39630f64eff17c1e647e328b6
Xwilarg <xwilarg@protonmail.com>
2022-05-07 09:41:30 +0200

    Add potion colors

---
+++ Assets/Scripts/Prop/PotionSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace GamedevGBG.Prop
+{
+    internal class PotionSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform[] _slots;
+        [SerializeField]
+        private GameObject _prefab;
+
+        [SerializeField]
+        private Material[] _mats;
+
+        private void Start()
+        {
+            foreach (var s in _slots)
+            {
+                var go = Instantiate(_prefab, s.transform.position, _prefab.transform.rotation);
+                var mesh = go.GetComponent<MeshRenderer>();
+                Material[] matArray = mesh.materials;
+                matArray[1] = _mats[Random.Range(0, _mats.Length)];
+                mesh.materials = matArray;
+            }
+        }
+    }
+}
