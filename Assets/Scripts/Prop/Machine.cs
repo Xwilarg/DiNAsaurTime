097188f475ce6e4867e8e6cb25a118d5d4c4984a
Xwilarg <xwilarg@protonmail.com>
2022-05-07 11:45:46 +0200

    Add liquid process

--- Assets/Scripts/Prop/Machine.cs
+++ Assets/Scripts/Prop/Machine.cs
@@ -1,4 +1,7 @@
 ﻿using GamedevGBG.Player;
+using GamedevGBG.SO;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 namespace GamedevGBG.Prop
@@ -6,11 +9,18 @@ namespace GamedevGBG.Prop
     public class Machine : MonoBehaviour
     {
         [SerializeField]
+        private MachineInfo _info;
+
+        [SerializeField]
         private Transform[] _slots;
+
+        [SerializeField]
+        private TMP_Text _progression;
 
         private Animator _anim;
 
         private int _fillIndex;
+        private float _timer = -1f;
 
         private void Awake()
         {
@@ -22,12 +32,41 @@ namespace GamedevGBG.Prop
             if (_fillIndex < _slots.Length)
             {
                 other.transform.position = _slots[_fillIndex].position;
+                other.transform.parent = _slots[_fillIndex].transform;
                 DragAndDrop.Instance.Drop();
                 _fillIndex++;
                 if (_fillIndex == _slots.Length)
                 {
                     _anim.SetBool("IsOpen", false);
-                    // TODO: Play close animation
+                    StartCoroutine(WaitAndProcess());
+                }
+            }
+        }
+
+        private IEnumerator WaitAndProcess()
+        {
+            yield return new WaitForSeconds(1f);
+            _timer = _info.ProcessTime;
+        }
+
+        private void Update()
+        {
+            if (_timer > 0f)
+            {
+                _timer -= Time.deltaTime;
+                if (_progression != null)
+                {
+                    _progression.text = $"{(_info.ProcessTime - _timer) * 100f / _info.ProcessTime:0.00}%";
+                }
+                if (_timer <= 0f)
+                {
+                    foreach (var slot in _slots)
+                    {
+                        Destroy(slot.GetChild(0).gameObject);
+                    }
+                    _fillIndex = 0;
+                    _progression.text = string.Empty;
+                    _anim.SetBool("IsOpen", true);
                 }
             }
         }
---
+++ Assets/Scripts/SO/MachineInfo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace GamedevGBG.SO
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/MachineInfo", fileName = "MachineInfo")]
+    public class MachineInfo : ScriptableObject
+    {
+        public float ProcessTime;
+    }
+}
