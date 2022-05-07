46b05063d0a3ea00b240abbbd86668bb22c56ae0
Xwilarg <xwilarg@protonmail.com>
2022-05-08 00:20:10 +0200

    Add part scripts

---
+++ Assets/Scripts/BodyPart/Head.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace GamedevGBG.BodyPart
+{
+    public class Head : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform Topping, Bowl;
+    }
+}
---
+++ Assets/Scripts/BodyPart/Torso.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace GamedevGBG.BodyPart
+{
+    public class Torso : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform CatHead, DinoHead, AlienHead, Tentacles, Topping;
+        [SerializeField]
+        private Transform CatTail, DinoTail, AlienTail;
+    }
+}
