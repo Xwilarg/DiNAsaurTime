a310823cd00def29eea1fb530e7074545b8067fb
Xwilarg <xwilarg@protonmail.com>
2022-05-07 15:02:12 +0200

    Improve robot controls

--- Assets/Scripts/Prop/RoboticArm.cs
+++ Assets/Scripts/Prop/RoboticArm.cs
@@ -16,21 +16,37 @@ namespace GamedevGBG.Prop
         [SerializeField]
         private Transform _arm, _hand, _finger;
 
+        private bool _isMoving;
+
         private int _targetIndex;
         private int _oldIndex;
 
         private float _oldDist;
+
+        private float _baseValue;
+        private float _objValue;
+        private float _actionTimer;
+
+        private enum ActionState
+        {
+            GoDown,
+            GoUp,
+            Done
+        }
+        private ActionState _currentAction = ActionState.Done;
 
         private void Start()
         {
             _targetIndex = _vialsDeposit.SlotCount;
             _oldIndex = _targetIndex - 1;
             _oldDist = float.PositiveInfinity;
+            _baseValue = _arm.transform.position.y;
+            _objValue = _baseValue - .05f;
         }
 
         public void GoLeft()
         {
-            if (_targetIndex > 0)
+            if (_targetIndex > 0 && _currentAction == ActionState.Done)
             {
                 _oldIndex = _targetIndex;
                 _targetIndex--;
@@ -40,7 +56,7 @@ namespace GamedevGBG.Prop
 
         public void GoRight()
         {
-            if (_targetIndex <_vialsDeposit.SlotCount)
+            if (_targetIndex <_vialsDeposit.SlotCount && _currentAction == ActionState.Done)
             {
                 _oldIndex = _targetIndex;
                 _targetIndex++;
@@ -50,6 +66,11 @@ namespace GamedevGBG.Prop
 
         public void DoAction()
         {
+            if (!_isMoving && _currentAction == ActionState.Done)
+            {
+                _currentAction = ActionState.GoDown;
+                _actionTimer = 0f;
+            }
         }
 
         private Vector3 GetPosition(int index)
@@ -69,6 +90,40 @@ namespace GamedevGBG.Prop
             {
                 _oldDist = dist;
                 _arm.transform.position += _arm.transform.right * (_targetIndex > _oldIndex ? 1f : -1f) * _speed * Time.deltaTime;
+                _isMoving = true;
+            }
+            else
+            {
+                _isMoving = false;
+            }
+
+            if (_currentAction == ActionState.GoDown)
+            {
+                _actionTimer += Time.deltaTime;
+                _arm.position = new Vector3(
+                    x: _arm.transform.position.x,
+                    y: Mathf.Lerp(_baseValue, _objValue, _actionTimer),
+                    z: _arm.transform.position.z
+                    );
+                if (_actionTimer >= 1f)
+                {
+                    _actionTimer = 0f;
+                    _currentAction = ActionState.GoUp;
+                }
+            }
+            else if (_currentAction == ActionState.GoUp)
+            {
+                _actionTimer += Time.deltaTime;
+                _arm.position = new Vector3(
+                    x: _arm.transform.position.x,
+                    y: Mathf.Lerp(_objValue, _baseValue, _actionTimer),
+                    z: _arm.transform.position.z
+                    );
+                if (_actionTimer >= 1f)
+                {
+                    _actionTimer = 0f;
+                    _currentAction = ActionState.Done;
+                }
             }
         }
     }
