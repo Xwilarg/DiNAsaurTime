5512be36a3bd6754d5e1075e624d61470bd23ffb
Xwilarg <xwilarg@protonmail.com>
2022-05-08 12:31:30 +0200

    Add trashbin

--- Assets/Scripts/Prop/Bin.cs
+++ Assets/Scripts/Prop/Bin.cs
@@ -4,5 +4,12 @@ namespace GamedevGBG.Prop
 {
     public class Bin : MonoBehaviour
     {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Draggable"))
+            {
+                Destroy(other.gameObject);
+            }
+        }
     }
 }
